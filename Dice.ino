7f94a4572d2ac86c4c67d13bfbeac4c7a3102e99
HamsterG <39745147+HamsterG@users.noreply.github.com>
2019-05-13 19:06:39 +0200

    Update Dice.ino

--- Dice.ino
+++ Dice.ino
@@ -1,14 +1,16 @@
-int buttonPin = 8;
 
+int buttonPin = 8;
+int blank = 6;
 int ledPins[7] = {5, 6, 7, 9, 10, 11, 12};
 
-int patterns[6][7] = {
+int patterns[7][7] = {
   {0, 0, 0, 1, 0, 0, 0}, //One
-  {0, 0, 0, 0, 1, 0, 0}, //Two
-  {0, 0, 0, 0, 0, 1, 0}, //Three
-  {0, 0, 0, 0, 0, 0, 1}, //Four
-  {0, 0, 1, 0, 0, 0, 0}, //Five
-  {0, 1, 0, 0, 0, 0, 0}, //Six
+  {0, 0, 1, 0, 0, 0, 1}, //Two
+  {0, 1, 0, 1, 0, 0, 1}, //Three
+  {0, 1, 1, 0, 1, 0, 1}, //Four
+  {0, 1, 1, 1, 1, 1, 0}, //Five
+  {1, 1, 1, 0, 1, 1, 1}, //Six
+  {0, 0, 0, 0, 0, 0, 0} //Blank
   };
 
 void setup() {
@@ -18,32 +20,27 @@ void setup() {
     }
     pinMode(buttonPin, INPUT);
     Serial.begin(9600);
+    randomSeed(analogRead(0));
   }
 
 void loop() {
-if(digitalRead(buttonPin)){
+if(digitalRead(buttonPin)) {
   roll();
-  delay(100);
+  delay(250);
   }
-
 
 }
 
- void show(int rndNum) {
-  for(int i = 0; i < ledPins; i++){
-    digitalWrite(ledPins[i], patterns[rndNum][i]);
-    delay(200);
-    }
+void roll() {
+  int result = 4;
+  result = random(0, 6);
+  delay(500);
+  show(result);
+  delay(500);
   }
 
-void roll() {
-  int rndNum = 0;
-  Serial.println(rndNum);
-  rndNum = random(0, 6);
-  Serial.println(rndNum);
-  delay(500);
-  show(rndNum);
-  delay(500);
-    }
-
-
+void show(int result) {
+ for(int i = 0; i < 7; i++) {
+  digitalWrite(ledPins[i], patterns[result][i]);
+  } 
+ }
